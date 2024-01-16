670cc5935a95186e8afc77b198a73144d8c6a4e6
Devon <41218582+Devon-Carroll@users.noreply.github.com>
2024-01-16 18:35:24 -0500

    Update theaters.service.js

--- src/theaters/theaters.service.js
+++ src/theaters/theaters.service.js
@@ -1,20 +1,28 @@
-const knex = require('../db/connection'); // Adjust the path as needed
+const knex = require("../db/connection");
+const reduceProperties = require("../utils/reduce-properties")
 
-function getAllTheatersWithMovies() {
-  return knex('theaters as t')
-    .leftJoin('movies_theaters as mt', 't.theater_id', 'mt.theater_id')
-    .leftJoin('movies as m', 'mt.movie_id', 'm.movie_id')
-    .then((theaters) => {
 
-      return theaters; // Return data from your database
-    });
+// Adds an array titled "movies", with information about the specific
+// movies currently showing in that theater.
+
+const reduceMovies = reduceProperties("theater_id", {
+  movie_id: ["movies", null, "movie_id"],
+  title: ["movies", null, "title"],
+  runtime_in_minutes: ["movies", null, "runtime_in_minutes"],
+  rating: ["movies", null, "rating"],
+  description: ["movies", null, "description"],
+  image_url: ["movies", null, "image_url"],
+  is_showing: ["movies", null, "is_showing"],
+})
+
+// Lists all the theaters, the adds the movies array/ properties
+
+async function list() {
+  return knex("theaters as t")
+    .join("movies_theaters as mt", "t.theater_id", "mt.theater_id")
+    .join("movies as m", "m.movie_id", "mt.movie_id")
+    .select("t.*", "m.*", "mt.*")
+    .then(reduceMovies)
 }
 
-function list() {
-  return knex("theaters").select("*");
-}
-
-module.exports = {
-  list,
-  getAllTheatersWithMovies,
-};
+module.exports = { list };
