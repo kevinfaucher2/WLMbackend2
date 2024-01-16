998cb9a48a5ff0ef2a81b76960a4eb5c89faca69
Devon <41218582+Devon-Carroll@users.noreply.github.com>
2024-01-16 18:35:49 -0500

    Update theaters.router.js

--- src/theaters/theaters.router.js
+++ src/theaters/theaters.router.js
@@ -1,9 +1,10 @@
-const router = require("express").Router();
+const router = require("express").Router({ mergeParams: true });
 const controller = require("./theaters.controller");
-const methodNotAllowed = require("../errors/methodNotAllowed");
+const methodNotAllowed = require("../errors/methodNotAllowed")
+// (/theaters) route
 
-router.route("/").get(controller.getAllTheaters).all(methodNotAllowed);
-
-
+router.route("/")
+    .get(controller.list)
+    .all(methodNotAllowed)
 
 module.exports = router;
