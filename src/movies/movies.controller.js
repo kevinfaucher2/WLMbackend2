615ab0e297a6bc953f60b8b3228e6e06d4115436
Devon <41218582+Devon-Carroll@users.noreply.github.com>
2024-01-16 18:39:33 -0500

    Update movies.controller.js

--- src/movies/movies.controller.js
+++ src/movies/movies.controller.js
@@ -1,51 +1,72 @@
-const { all } = require("./movies.router");
-const moviesService = require("./movies.service");
+const service = require("./movies.service")
+const asyncErrorBoundary = require("../errors/asyncErrorBoundary")
+
+// Validation functions
 
 async function movieExists(req, res, next) {
-  const movie = await moviesService.read(req.params.movieId);
-  if(movie) {
-    res.locals.movie = movie;
-    return next();
-  }
-  next({ status: 404, message: "Movie cannot be found."})
+
+    const movie = await service.read(req.params.movieId)
+
+    if (movie) {
+        res.locals.movie = movie
+        return next();
+    }
+    next({ status: 404, message: "Movie cannot be found." })
+}
+
+//-----------------------------------------------------------------//
+
+
+// GET (/movies) returns all movies
+// GET (/movies?is_showing=true) returns only movies that are currently showing in theaters
+
+async function list(req, res) {
+
+    const allMovies = await service.list();
+    const currentlyShowing = await service.currentlyShowing();
+    const isShowing = req.query.is_showing;
+    const data = isShowing ? currentlyShowing : allMovies;
+
+    res.json({ data });
+
+}
+
+// GET (/movies/:movieId) Read a specific movie by ID 
+
+async function read(req, res, next) {
+
+    const data = res.locals.movie;
+
+    res.json({ data: data })
+
 }
 
 
-async function list(req, res){
-  const data = await moviesService.list();
-  res.json({ data });
+// Display theaters currently showing movie (/movies/:movieId/theaters)
+
+async function listTheatersForMovie(req, res) {
+
+    const { movieId } = req.params;
+    const data = await service.listTheatersForMovie(movieId)
+
+    res.json({ data })
+
 }
 
-function read(req, res, next) {
-  const { movie: data } = res.locals;
-  res.json({ data })
+// Display reviews related to a movie, with critic info
+
+async function listReviewsForMovie(req, res) {
+
+    const { movieId } = req.params;
+    const data = await service.listReviewsForMovies(movieId)
+
+    res.json({ data })
 }
-
-async function listTheaterMovies(req, res) {
-  const data = await service.listTheaterMovies();
-  res.json({ data })
-}
-
-async function listReviews(req, res) {
-  const movieId = res.locals.movie.movie_id;
-  const reviews = await service.listReviews(movieId);
-  const allReviews = [];
-  for (let i = 0; i < reviews.length; i++) {
-    const review = reviews[i];
-    const critic = await service.getCritics(review.critic_id);
-    review.critic = critic[0];
-    allReviews.push(review);
-  }
-  res.status(200).json({ data: allReviews });
-}
-
-
-
 
 
 module.exports = {
-  list,
-  read: [movieExists, read],
-  listTheaterMovies,
-  listReviews,
-}
+    list,
+    read: [asyncErrorBoundary(movieExists), asyncErrorBoundary(read)],
+    theaters: [asyncErrorBoundary(movieExists), asyncErrorBoundary(listTheatersForMovie)],
+    reviews: [asyncErrorBoundary(movieExists), asyncErrorBoundary(listReviewsForMovie)]
+};
