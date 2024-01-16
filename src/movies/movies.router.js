1785d2b6ecc6b1bc24dcbde1677d3e9ece437eb6
Devon <41218582+Devon-Carroll@users.noreply.github.com>
2024-01-16 18:39:11 -0500

    Update movies.router.js

--- src/movies/movies.router.js
+++ src/movies/movies.router.js
@@ -1,14 +1,28 @@
-const router = require("express").Router();
-const controller = require("./movies.controller");
-const methodNotAllowed = require("../errors/methodNotAllowed");
+const router = require("express").Router({ mergeParams: true })
+const controller = require("./movies.controller")
+const methodNotAllowed = require("../errors/methodNotAllowed")
 
+// route for all movies (/movies)
+router.route("/")
+    .get(controller.list)
+    .all(methodNotAllowed)
 
-router.route("/").get(controller.list).all(methodNotAllowed);
+// route for a specific movie (/movies/:movieId)
 
-router.route("/:movieId([0-9]+)/theaters").get(controller.list).all(methodNotAllowed);
+router.route("/:movieId")
+    .get(controller.read)
+    .all(methodNotAllowed)
 
-router.route("/:movieId([0-9]+)").get(controller.read).all(methodNotAllowed);
+// route for displaying theaters currently showing a specific movie (/movies/:movieId/theaters)
 
-router.route("/:movieId([0-9]+)/reviews").get(controller.list).all(methodNotAllowed);
+router.route("/:movieId/theaters")
+    .get(controller.theaters)
+    .all(methodNotAllowed)
+
+// route for displaying reviews for a speicifc movieId
+
+router.route("/:movieId/reviews")
+    .get(controller.reviews)
+    .all(methodNotAllowed)
 
 module.exports = router;
